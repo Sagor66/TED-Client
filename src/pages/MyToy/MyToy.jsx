e0662e3ba6921bff2951bede937f68392c918e16
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-05-20 06:14:03 +0600

    get new toys from database and showed them in my-toys page

--- src/pages/MyToy/MyToy.jsx
+++ src/pages/MyToy/MyToy.jsx
@@ -1,9 +1,72 @@
-
+import { useLoaderData } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const MyToy = () => {
+  const newToys = useLoaderData();
+  console.log(newToys);
+
   return (
-    <div>
-      <h2>My toy</h2>
+    <div className="max-w-7xl mx-auto">
+      <h2 className="text-heading mt-16">My Toys</h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Sub-Category</th>
+              <th>Details</th>
+              <th>Seller Name</th>
+              <th>Seller E-mail</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Rating</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {newToys.map((toy) => (
+              <tr key={toy._id}>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img className="w-10" src={toy.photo} alt="" />
+                    </div>
+                  </div>
+                </td>
+                <td>{toy.name}</td>
+                <td>{toy.subCategory}</td>
+                <td>{toy.details}</td>
+                <td>{toy.sellerName}</td>
+                <td>{toy.sellerEmail}</td>
+                <td>{toy.price}</td>
+                <td>{toy.quantity}</td>
+                <td>{toy.rating}</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">
+                    <FontAwesomeIcon
+                      className="fa-xl text-green-500"
+                      icon={faPenToSquare}
+                    />
+                  </button>
+                </th>
+                <th>
+                  <button className="btn btn-ghost btn-xs">
+                    <FontAwesomeIcon
+                      className="fa-xl text-red-500"
+                      icon={faTrash}
+                    />
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
